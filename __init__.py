888634b6566dd743efbd2e76ddc6c9095689fc9e
GeJo28 <140900717+GeJo28@users.noreply.github.com>
2025-01-07 18:09:11 -0300

    __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 
@@ -18,5 +19,16 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message = 'Por favor, faça Login para acessar esta página'
 login_manager.login_message_category = 'alert-info'
+
+from comunidadeimpressionadora import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Base de Dados Criada")
+else:
+    print("Base de Dados já existe")
 
 from comunidadeimpressionadora import routes
